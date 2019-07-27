04518fc9d8955e2507722ee8e4a07f5ab50f1532
Tyler Phillips <thetylerjpjunk@icloud.com>
2019-07-26 23:13:36 -0400

    Initial Project Commit

---
+++ Spotify Mini Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spotify Mini Player
+//
+//  Created by Tyler P Admin on 7/26/19.
+//  Copyright © 2019 Tyler Phillips. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
